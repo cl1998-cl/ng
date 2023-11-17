4fe462a1c6a7db2d52b09801280fd5756227f93f
chenlonga <chenlonga@digiwin.com>
2023-11-17 18:40:39 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, inject, TemplateRef, ViewChild, ViewContainerRef} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {Overlay} from '@angular/cdk/overlay';
+import {ComponentPortal, TemplatePortal} from '@angular/cdk/portal';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +15,16 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 })
 export class AppComponent {
   title = 'myApp';
+
+  overlay: Overlay = inject(Overlay);
+  @ViewChild('templatePortalContent') templatePortalContent!: TemplateRef<unknown>;
+  _viewContainerRef = inject(ViewContainerRef);
+  open(){
+    const overlayRef = this.overlay.create();
+    const templatePortal = new TemplatePortal(
+      this.templatePortalContent,
+      this._viewContainerRef
+    );
+    overlayRef.attach(templatePortal);
+  }
 }
